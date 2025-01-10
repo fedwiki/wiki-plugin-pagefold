2c83a017a1c60bc40b2038f99889ceea98ecf331
Paul Rodwell <paul.rodwell@btinternet.com>
2025-01-10 16:42:40 +0000

    convert to JavaScript

--- src/client/pagefold.js
+++ src/client/pagefold.js
@@ -1,22 +1,26 @@
-###
+/*
  * Federated Wiki : Pagefold Plugin
  *
  * Licensed under the MIT license.
  * https://github.com/fedwiki/wiki-plugin-pagefold/blob/master/LICENSE.txt
-###
+ */
 
-# http://stackoverflow.com/questions/5214127/css-technique-for-a-horizontal-line-with-words-in-the-middle
+// http://stackoverflow.com/questions/5214127/css-technique-for-a-horizontal-line-with-words-in-the-middle
 
-emit = ($item, item) ->
-	$item.append """
+const emit = ($item, item) => {
+  $item.append(`
 		<div style="height: 10px; border-top: 2px solid lightgray; margin-top: 24px; text-align: center; position: relative; clear: both;">
 			<span style="position: relative; top: -.8em; background: white; display: inline-block; color: gray; ">
-				&nbsp; #{item.text} &nbsp;
+				&nbsp; ${item.text} &nbsp;
 			</span>
 		</div>
-	"""
+	`)
+}
 
-bind = ($item, item) ->
-	$item.on 'dblclick', () -> wiki.textEditor $item, item
+const bind = ($item, item) => {
+  $item.on('dblclick', () => wiki.textEditor($item, item))
+}
 
-window.plugins.pagefold = {emit, bind}
+if (typeof window !== 'undefined') {
+  window.plugins.pagefold = { emit, bind }
+}
